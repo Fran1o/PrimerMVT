84955db56af9dec0f2f24f98deba9653a84a3509
Fran1o <pankha994@gmail.com>
2022-11-08 21:26:36 -0300

    actualizando

--- codigo/familiaresapp/views.py
+++ codigo/familiaresapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template,Context
+from django.template import Template, Context
 from familiaresapp.models import Familia
 
 # Create your views here.
@@ -8,17 +7,5 @@ from familiaresapp.models import Familia
 def lista_familiares(request):
 
     familiares = Familia.objects.all()
-
-    archivo = open(r"C:\Users\Admin\Desktop\primermvt\codigo\familiaresapp\templates\inicio.html")
-
-    plantilla = Template(archivo.read())
-
-    archivo.close()
-
-    datos = {"datos_familiares": familiares}
-
-    contexto = Context(datos)
-
-    documento = plantilla.render(contexto)
     
-    return HttpResponse(documento)
+    return render(request, "inicio.html", {"mis_familiares": familiares})
--- codigo/primermvt/settings.py
+++ codigo/primermvt/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'primermvt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r"C:\Users\Admin\Desktop\primermvt\codigo\familiaresapp\templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
